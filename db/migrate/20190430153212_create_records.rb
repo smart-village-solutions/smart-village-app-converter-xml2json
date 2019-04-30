a1288acbed4cc090c829ece9e5e50b3897fe5ef7
Marco Metz <m.metz@ikusei.de>
2019-04-30 17:41:47 +0200

    Initial commit

---
+++ db/migrate/20190430153212_create_records.rb
@@ -0,0 +1,11 @@
+class CreateRecords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :records do |t|
+      t.string :external_id
+      t.jsonb :json_data
+      t.text :xml_data
+
+      t.timestamps
+    end
+  end
+end
