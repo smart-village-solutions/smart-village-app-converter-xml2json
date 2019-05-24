ebfb62e5d482f59701ba9d3512607c0df92d64c7
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-24 17:47:11 +0200

    Add tour parser to poi record
    * add methods to parse all relevant data from tour nodes connected to single pois ("Pois with a tours tag")
    
    * add specs and example_data for modified poi_record class

--- spec/models/record/poi_record_spec.rb
+++ spec/models/record/poi_record_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PoiRecord, type: :model do
   describe "import" do
-    let(:xml_raw_data) { File.read("doc/tmb_poi.xml") }
-    let(:poi_hash) { JSON.parse(File.read("doc/tmb_poi.json")) }
+    let(:xml_raw_data) { File.read("doc/tmb.xml") }
+    let(:poi_hash) { JSON.parse(File.read("doc/pois_tours.json")) }
     let(:poi) { PoiRecord.new(current_user: nil) }
 
     it "stores a xml in xml_data" do
@@ -18,12 +18,16 @@ RSpec.describe PoiRecord, type: :model do
       expect(poi.convert_to_json(hash_data)).to eq("{\"foo\":\"bar\"}")
     end
 
+    # ToDo Refactor according to cc rspec rules
     it "converts a xml to hash" do
       poi.xml_data = xml_raw_data
       result = poi.convert_xml_to_hash
 
+      expect(result).to be_a(Hash)
       expect(result[:point_of_interests].present?).to eq(true)
-      expect(result[:point_of_interests].count).to eq(241)
+      expect(result[:tours].present?).to eq(true)
+      expect(result[:point_of_interests].count).to eq(195)
+      expect(result[:tours].count).to eq(47)
     end
   end
 end
