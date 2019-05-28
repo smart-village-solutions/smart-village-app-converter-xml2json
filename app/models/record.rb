5c1c99f7110ed8100794d94c181a4c586668fcf6
Marco Metz <m.metz@ikusei.de>
2019-05-28 10:54:03 +0200

    current_user als Parameterübergabe

--- app/models/record.rb
+++ app/models/record.rb
@@ -20,10 +20,10 @@ class Record < ApplicationRecord
     raise "Abstract Method"
   end
 
-  def data_provider
-    return {} if @current_user.blank?
+  def data_provider(current_user)
+    return {} if current_user.blank?
 
-    @current_user.fetch(:data_provider, {})
+    current_user.fetch(:data_provider, {})
   end
 end
 
