9861229a68666862d034c919283b65ea0d8e8f00
Sebastian Peter <sebastian.peter@ikusei.de>
2019-06-24 17:04:00 +0200

    Add Translation Service
    * use new translation service class to define a method where english weekdays are parsed to have german weekdays

---
+++ app/services/translation.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+class Translation
+  def self.weekdays(opening_day)
+    day = opening_day.at_xpath("weekday").try(:text)
+
+    case day
+    when "monday"
+      "Montag"
+    when "tuesday"
+      "Dienstag"
+    when "wednesday"
+      "Mittwoch"
+    when "thursday"
+      "Donnerstag"
+    when "friday"
+      "Freitag"
+    when "saturday"
+      "Samstag"
+    when "sunday"
+      "Sonntag"
+    when "sunday_holiday"
+      "Sonntag"
+    when "newYear"
+      "01.01. (Neujahr)"
+    when "laborDay"
+      "01.05. (Tag der Arbeit)"
+    when "germanUnity"
+      "03.10. (Tag der deutschen Einheit)"
+    when "christmasEve"
+      "24.12. (Heiligabend)"
+    when "firstChristmasDay"
+      "25.12. (1. Weihnachtsfeiertag)"
+    when "secondChristmasDay"
+      "26.12. (2. Weihnachtsfeiertag)"
+    when "silvester"
+      "31.12. (Silvester)"
+    when "goodFriday"
+      "Karfreitag"
+    when "easterSunday"
+      "Ostersonntag"
+    when "easterMonday"
+      "Ostermontag"
+    when "ascensionDay"
+      "Himmelfahrt"
+    when "whitSunday"
+      "Pfingstsonntag"
+    when "whitMonday"
+      "Pfingstmontag"
+    when "reformationDay"
+      "31.10. (Reformationstag)"
+    end
+  end
+
+end
