ce886984273b2712fe0d4844f4695e596ecca39c
Sebastian Peter <sebastian.peter@ikusei.de>
2019-08-12 21:15:12 +0200

    Add route/endpoint to manually import pois

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+
+  def import_poi
+    Importer.new(:poi)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   match "oauth/confirm_access", to: "oauth#confirm_access", via: [:get, :post]
-
+  get "/import_poi", to: "application#import_poi"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
