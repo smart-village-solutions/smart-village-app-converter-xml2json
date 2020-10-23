96a9d9a9eaa64ddaae09ee845f4efdc26513171f
Marco Metz <mm@tpwd.de>
2020-10-23 11:52:41 +0200

    Update postgres DB Server
    - switch to new postgres version
    - use bundle exec on rake task
    - switch to ruby-nginx docker image
    - switch cron service
    - nginx stack removed
    
    SVA-5

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,9 +21,59 @@ set :output, "/var/log/cron.log"
 set :job_template, "/bin/sh -l -c ':job'"
 set :environment, ENV["RAILS_ENV"]
 
-every 1.day do
-  runner "Importer.new(:poi)"
-  runner "Importer.new(:event)"
+every 1.day, at: "20:05 am" do
+  runner "Importer.new(:poi, :int_development)"
+  runner "Importer.new(:event, :int_development)"
+end
+
+every 1.day, at: "21:05 am" do
+  runner "Importer.new(:poi, :bb_michendorf)"
+  runner "Importer.new(:event, :bb_michendorf)"
+end
+
+every 1.day, at: "22:05 am" do
+  runner "Importer.new(:poi, :bb_kyritz)"
+  runner "Importer.new(:event, :bb_kyritz)"
+end
+
+every 1.day, at: "23:05 am" do
+  runner "Importer.new(:poi, :herzberg_elster)"
+  runner "Importer.new(:event, :herzberg_elster)"
+end
+
+every 1.day, at: "00:05 am" do
+  runner "Importer.new(:poi, :bad_belzig)"
+  runner "Importer.new(:event, :bad_belzig)"
+end
+
+every 1.day, at: "01:05 am" do
+  runner "Importer.new(:poi, :eisenhuettenstadt)"
+  runner "Importer.new(:event, :eisenhuettenstadt)"
+end
+
+every 1.day, at: "02:05 am" do
+  runner "Importer.new(:poi, :amt_schlieben)"
+  runner "Importer.new(:event, :amt_schlieben)"
+end
+
+every 1.day, at: "03:05 am" do
+  runner "Importer.new(:poi, :birkenwerder)"
+  runner "Importer.new(:event, :birkenwerder)"
+end
+
+every 1.day, at: "04:05 am" do
+  runner "Importer.new(:poi, :bb_falkenberg_elster)"
+  runner "Importer.new(:event, :bb_falkenberg_elster)"
+end
+
+every 1.day, at: "05:05 am" do
+  runner "Importer.new(:poi, :bb_frankfurt)"
+  runner "Importer.new(:event, :bb_frankfurt)"
+end
+
+every 1.day, at: "06:05 am" do
+  runner "Importer.new(:poi, :bb_gransee)"
+  runner "Importer.new(:event, :bb_gransee)"
 end
 
 # Learn more: http://github.com/javan/whenever
